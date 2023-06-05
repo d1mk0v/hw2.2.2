01c7a14dbf78e72399258637da016510b0573cd0
d1mk0v <79874763588@ya.ru>
2023-06-05 19:24:41 +0500

    1

---
+++ src/Bicycle.java
@@ -0,0 +1,31 @@
+public class Bicycle {
+
+    private String modelName;
+    private int wheelsCount;
+
+    public Bicycle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+
+    public void updateTyre() {
+
+        System.out.println("Меняем покрышку");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,99 +1,50 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашка 2.3. ООП часть 2. Интерфейсы и полиморфизм");
-        Car car = new Car();
-        Car car2 = new Car();
-        car.modelName = "car1";
-        car2.modelName = "car2";
-        car.wheelsCount = 4;
-        car2.wheelsCount = 4;
 
-        Truck truck = new Truck();
-        Truck truck2 = new Truck();
-        truck.modelName = "truck1";
-        truck2.modelName = "truck2";
-        truck.wheelsCount = 6;
-        truck2.wheelsCount = 8;
+        Car toyota = new Car("Toyota", 4);
+        Car ferrari = new Car("Ferrari", 4);
 
-        Bicycle bicycle = new Bicycle();
-        Bicycle bicycle2 = new Bicycle();
-        bicycle.modelName = "bicycle1";
-        bicycle2.modelName = "bicycle2";
-        bicycle.wheelsCount = 2;
-        bicycle2.wheelsCount = 2;
+
+        Truck volvo = new Truck("Volvo", 6);
+        Truck man = new Truck("MAN", 8);
+
+
+        Bicycle giant = new Bicycle("GIANT", 2);
+        Bicycle trek = new Bicycle("TREK", 2);
+
 
         ServiceStation station = new ServiceStation();
-        station.check(car, null, null);
-        station.check(car2, null, null);
-        station.check(null, bicycle, null);
-        station.check(null, bicycle2, null);
-        station.check(null, null, truck);
-        station.check(null, null, truck2);
-    }
-}
-
-public class Bicycle {
-
-    public String modelName;
-    public int wheelsCount;
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
-    }
-}
-
-public class Car {
-
-    public String modelName;
-    public int wheelsCount;
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
-    }
-
-    public void checkEngine() {
-        System.out.println("Проверяем двигатель");
+        station.check(toyota, null, null);
+        station.check(ferrari, null, null);
+        station.check(null, giant, null);
+        station.check(null, trek, null);
+        station.check(null, null, volvo);
+        station.check(null, null, man);
     }
 }
 
 public class ServiceStation {
     public void check(Car car, Bicycle bicycle, Truck truck) {
         if (car != null) {
-            System.out.println("Обслуживаем " + car.modelName);
-            for (int i = 0; i < car.wheelsCount; i++) {
+            System.out.println("Обслуживаем " + car.getModelName());
+            for (int i = 0; i < car.getWheelsCount(); i++) {
                 car.updateTyre();
             }
             car.checkEngine();
         } else if (truck != null) {
-            System.out.println("Обслуживаем " + truck.modelName);
-            for (int i = 0; i < truck.wheelsCount; i++) {
+            System.out.println("Обслуживаем " + truck.getModelName());
+            for (int i = 0; i < truck.getWheelsCount(); i++) {
                 truck.updateTyre();
             }
             truck.checkEngine();
             truck.checkTrailer();
         } else if (bicycle != null) {
-            System.out.println("Обслуживаем " + bicycle.modelName);
-            for (int i = 0; i < bicycle.wheelsCount; i++) {
+            System.out.println("Обслуживаем " + bicycle.getModelName());
+            for (int i = 0; i < bicycle.getWheelsCount(); i++) {
                 bicycle.updateTyre();
             }
         }
     }
 }
 
-public class Truck {
-
-    public String modelName;
-    public int wheelsCount;
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
-    }
-
-    public void checkEngine() {
-        System.out.println("Проверяем двигатель");
-    }
-
-    public void checkTrailer() {
-        System.out.println("Проверяем прицеп");
-    }
-}
